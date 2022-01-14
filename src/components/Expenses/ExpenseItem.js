20c3c6dfa628a4df03d4553b94c5eacbdc20e8e6
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-01-14 18:04:55 -0300

    Assignment 4: Time to Practice: Conditional Content

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
   // const clickHandler = () => {
   //   setTitle("UPDATED!!!");
@@ -16,7 +16,6 @@ function ExpenseItem(props) {
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-          <h2>{title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
         {/* <button onClick={clickHandler}>Change Title</button> */}
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseForm from './ExpenseForm'
 
 import './NewExpense.css'
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false)
+
   const saveEpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -12,11 +14,19 @@ const NewExpense = (props) => {
 
     console.log(expenseData)
     props.onAddExpense(expenseData)
+    setIsEditing(false)
   }
+
+  const startEditingHandler = () => {
+    setIsEditing(true)
+  }
+
+  const stopEditingHandler = () => { setIsEditing(false) }
 
   return(
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveEpenseDataHandler} />
+      {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveEpenseDataHandler} onCancel={stopEditingHandler} />}
     </div>
   )
 }
