e65fb555537b8fd312f877a216f8ea69aeac2ea5
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-04-17 20:28:56 -0300

    57. Adding Two-Way Binding

--- section_3_and_4/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ section_3_and_4/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -27,41 +27,46 @@ const ExpenseForm = () => {
   }
 
   const submitHandler = (event) => {
-    // event.preventDefault();
+    event.preventDefault();
 
-    // const expenseData = {
-    //   title: enteredTitle,
-    //   amount: enteredAmount,
-    //   date: new Date(enteredDate)
-    // }
+    const expenseData = {
+      title: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
+      date: new Date(userInput.enteredDate)
+    }
 
-    // console.log(userInput)
-    // setEnteredTitle('')
-    // setEnteredAmount('')
-    // setEnteredDate('')
+    console.log(expenseData)
+
+    setUserInput(
+      {
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+      }
+    )
   }
 
   return(
-    <form >
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}/>
+          <input type="text" value={userInput.enteredTitle} onChange={titleChangeHandler}/>
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input type="number" min="0.01" step="0.01" value={userInput.enteredAmount} onChange={amountChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2023-01-01" onChange={dateChangeHandler} />
+          <input type="date" min="2023-01-01" value={userInput.enteredDate} onChange={dateChangeHandler} />
         </div>
       </div>
 
       <div className="new-expense__actions">
-        <button type="submit" onSubmit={submitHandler}>Add Expense</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   )
