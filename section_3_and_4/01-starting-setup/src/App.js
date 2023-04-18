b7ec679d951ca29a84b1386f7383136a0102c6f1
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-04-17 21:02:48 -0300

    59. Lifting The State Up

--- section_3_and_4/01-starting-setup/src/App.js
+++ section_3_and_4/01-starting-setup/src/App.js
@@ -33,9 +33,14 @@ const App = () => {
   //   React.createElement(Expenses, { items: expenses })
   // );
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  }
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- section_3_and_4/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ section_3_and_4/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './ExpenseForm.css'
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [userInput, setUserInput] = useState({
     enteredTitle: '',
     enteredAmount: '',
@@ -35,7 +35,7 @@ const ExpenseForm = () => {
       date: new Date(userInput.enteredDate)
     }
 
-    console.log(expenseData)
+    props.onSaveExpenseData(expenseData)
 
     setUserInput(
       {
