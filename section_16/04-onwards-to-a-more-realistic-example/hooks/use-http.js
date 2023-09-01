e941d0eddadea62118eb19913782c8b838ffc33d
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-08-31 21:33:54 -0300

    225. Building a Custom Http Hook

---
+++ section_16/04-onwards-to-a-more-realistic-example/hooks/use-http.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+const useHttp = (requestConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(
+        requestConfig.url,
+        {
+          method: requestConfig.method,
+          body: JSON.stringify({ text: taskText }),
+          headers: requestConfig.headers,
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      applyData(data)
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading,
+    error,
+    sendRequest
+  }
+}
+
+export default useHttp
