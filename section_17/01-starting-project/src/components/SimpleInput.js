87b8e61f359709814d8e8a4e5282d0c57de7f277
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-09-11 21:47:15 -0300

    237. React To Lost Focus

--- section_17/01-starting-project/src/components/SimpleInput.js
+++ section_17/01-starting-project/src/components/SimpleInput.js
@@ -16,6 +16,17 @@ const SimpleInput = (props) => {
     setEnteredName(event.target.value);
   };
 
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true)
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+  };
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
@@ -42,7 +53,7 @@ const SimpleInput = (props) => {
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} onBlur={nameInputBlurHandler} />
         {nameInputIsInvalid && <p className='error-text'> Name must not be empty. </p>}
       </div>
       <div className="form-actions">
