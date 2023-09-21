3929417500872f556625ea647d09f24ebbe7372c
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-09-21 20:19:53 -0300

    265. More Redux Basics

---
+++ section_19/redux-demo.js
@@ -0,0 +1,31 @@
+const redux = require('redux')
+
+const initialState = {
+  counter: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1
+    }
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1
+    }
+  }
+}
+
+const store = redux.createStore(counterReducer)
+
+const counterSubscriber = () => {
+  const latestState = store.getState()
+  console.log(latestState)
+}
+
+store.subscribe(counterSubscriber)
+
+store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'decrement' })
