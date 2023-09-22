07c21ce24837c2b77f168196c7e8467b2923a24e
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-09-22 18:16:10 -0300

    278. Migrating Everything To Redux Toolkit

--- section_19/01-starting-project/src/components/Counter.js
+++ section_19/01-starting-project/src/components/Counter.js
@@ -1,5 +1,7 @@
 // import { Component } from 'react';
+
 import { useSelector, useDispatch } from 'react-redux';
+import { counterActions } from '../store';
 
 import classes from './Counter.module.css';
 
@@ -9,23 +11,23 @@ const Counter = () => {
   const showCounter = useSelector(state => state.showCounter)
 
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' })
+    dispatch(counterActions.toggleCounter())
   };
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' })
+    dispatch(counterActions.increment())
   };
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 5 })
+    dispatch(counterActions.increase(5))
   };
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' })
+    dispatch(counterActions.decrement())
   };
 
   const decreaseHandler = () => {
-    dispatch({ type: 'decrease', amount: 5 })
+    dispatch(counterActions.decrease(5))
   };
 
   return (
--- section_19/01-starting-project/src/store/index.js
+++ section_19/01-starting-project/src/store/index.js
@@ -17,10 +17,10 @@ const counterSlice = createSlice({
       state.counter--
     },
     increase(state, action) {
-      state.counter = state.counter + action.amount
+      state.counter = state.counter + action.payload
     },
     decrease(state, action) {
-      state.counter = state.counter - action.amount
+      state.counter = state.counter - action.payload
     },
     toggleCounter(state) {
       state.showCounter = !state.showCounter
@@ -31,5 +31,7 @@ const counterSlice = createSlice({
 const store = configureStore({
   reducer: counterSlice.reducer
 });
+
+export const counterActions = counterSlice.actions
 
 export default store
