fabdec97d597efe1641ebce7488b5f08b3daa056
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-09-27 21:38:44 -0300

    294. Using an Action Creator Thunk

--- section_20/01-starting-project/src/App.js
+++ section_20/01-starting-project/src/App.js
@@ -5,7 +5,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './store/ui-slice';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -17,45 +17,12 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!'
-      }));
-
-      const response = await fetch('https://hooks-react-test-fa578-default-rtdb.firebaseio.com//cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart)
-      })
-
-      if (!response.ok) {
-        dispatch(uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!'
-        }));
-      }
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success!',
-        message: 'Sent cart data successfully!'
-      }));
-    }
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart data failed!'
-      }));
-    })
+    dispatch(sendCartData(cart))
   }, [cart, dispatch]);
 
   return (
