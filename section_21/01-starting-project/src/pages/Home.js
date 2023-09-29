2ab9793304b423ddf2f911879c07c1593498a865
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-09-28 21:53:42 -0300

    310. Navigating Programmatically

--- section_21/01-starting-project/src/pages/Home.js
+++ section_21/01-starting-project/src/pages/Home.js
@@ -1,10 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/products');
+  }
+
   return(
     <>
       <h1>My home page</h1>
       <Link to="/products">Products</Link>
+      <button onClick={handleClick}>To Products</button>
     </>
   );
 }
