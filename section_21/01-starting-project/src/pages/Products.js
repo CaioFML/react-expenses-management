247f9aa58524cd211c6d9e0ffba42a0fbbb28588
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-09-28 21:17:12 -0300

    304. Adding a Second Route

---
+++ section_21/01-starting-project/src/pages/Products.js
@@ -0,0 +1,14 @@
+function ProductsPage() {
+  return (
+    <div>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Carpet</li>
+        <li>An Online Course</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ProductsPage;
