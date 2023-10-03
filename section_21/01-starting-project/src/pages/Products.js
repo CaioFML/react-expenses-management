14f9163e0394629a08f722f0f9ce90a816b44431
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-10-02 22:25:39 -0300

    312. Adding Links for Dynamic Routes

--- section_21/01-starting-project/src/pages/Products.js
+++ section_21/01-starting-project/src/pages/Products.js
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function ProductsPage() {
   return (
     <div>
       <h1>The Products Page</h1>
       <ul>
-        <li>A Book</li>
-        <li>A Carpet</li>
-        <li>An Online Course</li>
+        <li><Link to='/products/product-1'>A Book</Link></li>
+        <li><Link to='/products/product-2'>A Carpet</Link></li>
+        <li><Link to='/products/product-3'>An Online Course</Link></li>
       </ul>
     </div>
   );
