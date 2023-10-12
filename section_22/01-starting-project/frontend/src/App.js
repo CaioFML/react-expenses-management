00bb4afc739a1cad53b6b811483fd3f641d65619
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-10-11 23:37:28 -0300

    348. Implementing the Auth Action

--- section_22/01-starting-project/frontend/src/App.js
+++ section_22/01-starting-project/frontend/src/App.js
@@ -13,7 +13,7 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
-import AuthenticationPage from './pages/Authentication';
+import AuthenticationPage, { action as AuthenticationAction } from './pages/Authentication';
 
 const router = createBrowserRouter([
   {
@@ -62,7 +62,8 @@ const router = createBrowserRouter([
       },
       {
         path: 'auth',
-        element: <AuthenticationPage />
+        element: <AuthenticationPage />,
+        action: AuthenticationAction
       }
     ],
   },
