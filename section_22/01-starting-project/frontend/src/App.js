a2be70dddfed31278603d96a462dda0207f4a375
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-10-12 00:19:35 -0300

    352. Adding User Logout

--- section_22/01-starting-project/frontend/src/App.js
+++ section_22/01-starting-project/frontend/src/App.js
@@ -14,6 +14,7 @@ import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, { action as AuthenticationAction } from './pages/Authentication';
+import { action as LogoutAction } from './pages/Logout';
 
 const router = createBrowserRouter([
   {
@@ -64,6 +65,10 @@ const router = createBrowserRouter([
         path: 'auth',
         element: <AuthenticationPage />,
         action: AuthenticationAction
+      },
+      {
+        path: 'logout',
+        action: LogoutAction
       }
     ],
   },
