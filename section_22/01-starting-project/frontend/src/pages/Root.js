a3af8a8a95cfb14ec25825b1564b3b1cfa23ba22
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-10-12 00:39:22 -0300

    356. Adding Automatic Logout

--- section_22/01-starting-project/frontend/src/pages/Root.js
+++ section_22/01-starting-project/frontend/src/pages/Root.js
@@ -1,9 +1,21 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit()
+
+  useEffect(() => {
+    if (!token) {
+      return
+    }
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' })
+    }, 1 * 60 * 60 * 1000)
+  }, [token])
 
   return (
     <>
