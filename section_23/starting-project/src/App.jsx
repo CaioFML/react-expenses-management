e5a96f34be49e5ce8334c646713362c76d2fd6de
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-11-07 21:24:09 -0300

    517. Acting on Mutation Success & Invalidating Queries

--- section_23/starting-project/src/App.jsx
+++ section_23/starting-project/src/App.jsx
@@ -4,7 +4,8 @@ import {
   createBrowserRouter,
 } from 'react-router-dom';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './util/http.js';
 
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
@@ -38,8 +39,6 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-
-const queryClient = new QueryClient();
 
 function App() {
   return(
