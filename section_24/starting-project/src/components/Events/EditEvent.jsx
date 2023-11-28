cd8e7ee95b8fc0411c3fdbb6fa0f5cf5e6f50044
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2023-11-27 21:17:01 -0300

    522. React Query Advantages In Action

--- section_24/starting-project/src/components/Events/EditEvent.jsx
+++ section_24/starting-project/src/components/Events/EditEvent.jsx
@@ -1,20 +1,52 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+
+import { fetchEvent } from '../../util/http.js';
 
 export default function EditEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const params = useParams();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['event', params.id],
+    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+  })
+
+  function handleSubmit(formData) { }
 
   function handleClose() {
     navigate('../');
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  let content;
+
+  if (isPending) {
+    content = <div className="center">
+      <LoadingIndicator />
+    </div>
+  }
+
+  if (isError) {
+    content = (
+      <>
+        <ErrorBlock title='Failed to load event' message={error.info?.message || 'Failed to fetch data'} />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link>
+        </div>
+      </>
+    )
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +54,12 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
+    )
+  }
+
+  return (
+    <Modal onClose={handleClose}>
+      {content}
     </Modal>
   );
 }
