621595791d1bc2cd1ae2676df82eaaf2933cb0c2
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-03-07 19:46:45 -0300

    434. Handling "Not Found" States

---
+++ section_25/05-onwards-foodies-starting-project/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Page not found</h1>
+      <p>Try going back to your previous page.</p>
+    </main>
+  )
+}
