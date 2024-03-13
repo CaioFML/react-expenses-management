0b9e20b0743a1c33221cc0f516c921682629eef8
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-03-13 19:32:30 -0300

    442. Storing Server Actions in Separate Files

--- section_25/05-onwards-foodies-starting-project/app/meals/share/page.js
+++ section_25/05-onwards-foodies-starting-project/app/meals/share/page.js
@@ -1,22 +1,8 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/app/lib/actions';
 
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    'use server';
-
-    const mealData = {
-      title: formData.get('title'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email')
-    };
-
-    console.log(mealData)
-  }
-
   return (
     <>
       <header className={classes.header}>
