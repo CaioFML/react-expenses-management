05cf01cc631c14f02bc7b767fd737db30f32ea03
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-03-14 18:47:01 -0300

    446. Adding Server-Side Input Validation

--- section_25/05-onwards-foodies-starting-project/app/lib/actions.js
+++ section_25/05-onwards-foodies-starting-project/app/lib/actions.js
@@ -3,6 +3,10 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+function isInvalidText(text) {
+  return !text || text.trim() === '';
+}
+
 export async function shareMeal(formData) {
   const mealData = {
     title: formData.get('title'),
@@ -12,6 +16,20 @@ export async function shareMeal(formData) {
     creator: formData.get('name'),
     creator_email: formData.get('email')
   };
+
+  if (
+    isInvalidText(mealData.title) ||
+    isInvalidText(mealData.summary) ||
+    isInvalidText(mealData.instructions) ||
+    isInvalidText(mealData.image) ||
+    isInvalidText(mealData.creator) ||
+    isInvalidText(mealData.creator_email) ||
+    !mealData.creator_email.includes('@') ||
+    !mealData.image ||
+    !mealData.image.size === 0
+  ) {
+    throw new Error('Invalid input');
+  }
 
   await saveMeal(mealData);
   redirect('/meals');
