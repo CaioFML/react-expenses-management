c0a2b7f656847b23b98dd8262ce0565f40dba2a3
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-04-16 18:30:47 -0300

    461. Extracting Dynamic Parameter Values

--- section_25/01-starting-project/pages/news/[newsId].js
+++ section_25/01-starting-project/pages/news/[newsId].js
@@ -1,5 +1,9 @@
-export default function DetailPage({ params }) {
+import { useRouter } from 'next/router'
+
+export default function DetailPage() {
+  const router = useRouter()
+
   return(
-    <h1>The News Detail Page</h1>
+    <h1>The News Detail Page, {router.query.newsId}</h1>
   )
 }
