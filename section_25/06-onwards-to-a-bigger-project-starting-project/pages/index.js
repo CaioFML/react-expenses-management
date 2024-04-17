cdb7f383c13d239e6457e9464369d3723c260299
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-04-17 19:19:03 -0300

    465. Outputting a List of Meetups

--- section_25/06-onwards-to-a-bigger-project-starting-project/pages/index.js
+++ section_25/06-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -1,11 +1,26 @@
+import MeetUpList from '../components/meetups/MeetUpList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://www.thespruceeats.com/thmb/5dJvJ9QsRqQV0yFJjJzWJY6b9a4=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-1197840992-5c8f7f7f46e0fb0001f8a6d0.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!'
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://www.thespruceeats.com/thmb/5dJvJ9QsRqQV0yFJjJzWJY6b9a4=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-1197840992-5c8f7f7f46e0fb0001f8a6d0.jpg',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup!'
+  }
+];
+
 export default function Home() {
   return (
-    <div>
-      <h1>Starting a new project</h1>
-      <p>
-        This is the first page of the new project. It's time to start building
-        something new!
-      </p>
-    </div>
+    <>
+      <MeetUpList meetups={DUMMY_MEETUPS} />
+    </>
   );
 }
