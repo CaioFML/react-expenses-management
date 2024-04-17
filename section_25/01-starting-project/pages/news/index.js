80fa76dcc14e663e7e471c2a5bb5fce8931034c5
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-04-17 18:24:03 -0300

    462. Linking Between Pages

--- section_25/01-starting-project/pages/news/index.js
+++ section_25/01-starting-project/pages/news/index.js
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 export default function NewsPage() {
   return(
-    <h1>The News Page</h1>
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li><Link href='/news/test'>Test</Link></li>
+      </ul>
+    </>
+
   )
 }
