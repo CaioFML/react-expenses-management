473cf59de87f207c64d9d89fc8ab459127f565ad
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-04-22 19:07:15 -0300

    466. Adding the New Meetup Form

--- section_25/06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
+++ section_25/06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
@@ -1,7 +1,13 @@
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
 export default function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    console.log(meetupData);
+  }
+
   return (
-    <div>
-      <h1>Add a New Meetup</h1>
-    </div>
+    <>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
   );
 }
