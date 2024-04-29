2e9ef874660877c6cc5a6aef4b2e08ce1c728ef3
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-04-29 16:35:59 -0300

    474. Working with Params for SSG Data Fetching

--- section_25/06-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
+++ section_25/06-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
@@ -5,3 +5,19 @@ export default function MeetupDetails() {
     <MeetupDetail image="https://i0.wp.com/landlopers.com/wp-content/uploads/2016/08/Schloss-Hof-Austria.jpg?resize=1044%2C762&ssl=1" title="A First Meetup" address="Some address 5, 12345 Some City" description="This is a first meetup!" />
   );
 }
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image: 'https://i0.wp.com/landlopers.com/wp-content/uploads/2016/08/Schloss-Hof-Austria.jpg?resize=1044%2C762&ssl=1',
+        id: meetupId,
+        title: 'A First Meetup',
+        address: 'Some address 5, 12345 Some City',
+        description: 'This is a first meetup!'
+      }
+    }
+  };
+}
