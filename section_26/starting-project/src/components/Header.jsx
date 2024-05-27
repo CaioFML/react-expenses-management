14166b8ad96b8a2a5cc91680d7045e7c49b5e2e7
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-05-27 17:33:14 -0300

    495. Making Elements "Pop" With Hover Animations

--- section_26/starting-project/src/components/Header.jsx
+++ section_26/starting-project/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import NewChallenge from './NewChallenge.jsx';
 
@@ -16,15 +16,18 @@ export default function Header() {
 
   return (
     <>
-    <AnimatePresence>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
-    </AnimatePresence>
+      <AnimatePresence>
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      </AnimatePresence>
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: 'spring', stiffness: 500 }}
+          onClick={handleStartAddNewChallenge} className="button">
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
