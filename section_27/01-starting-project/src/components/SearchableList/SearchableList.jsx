6e5106d82c1de0daa6d4ceddc9e42b8c9c7d9b1e
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-06-18 18:55:16 -0300

    519. Working with Debouncing

--- section_27/01-starting-project/src/components/SearchableList/SearchableList.jsx
+++ section_27/01-starting-project/src/components/SearchableList/SearchableList.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 export default function SearchableList({ items, itemKeyFn, children }) {
+  const lastChange = useRef()
+
   const [searchTerm, setSearchTerm] = useState('');
 
   const searchResults = items.filter((item) => JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()))
 
   function handleChange(event) {
-    setSearchTerm(event.target.value)
+    if (lastChange.current) {
+      clearTimeout(lastChange.current)
+    }
+
+    lastChange.current = setTimeout(() => {
+      lastChange.current = null
+      setSearchTerm(event.target.value)
+    }, 500)
   }
 
   return (
