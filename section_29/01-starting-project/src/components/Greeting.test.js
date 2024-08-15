cbc2d9a99a8f29ec1e7c0d2d6be69aa1ea224529
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-08-15 18:42:59 -0300

    543. Testing User Interaction & State

--- section_29/01-starting-project/src/components/Greeting.test.js
+++ section_29/01-starting-project/src/components/Greeting.test.js
@@ -1,10 +1,29 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 describe('Greeting component', () => {
   test('renders Greeting', () => {
     render(<Greeting />);
     const linkElement = screen.getByText(/Good to see you/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('renders "Changed!" when button is clicked', () => {
+    render(<Greeting />);
+
+    // const buttonElement = screen.getByRole('button');
+    // buttonElement.click();
+
+    // OR
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement)
+
+    const linkElement = screen.getByText(/Changed!/i);
+    const textElement = screen.queryByText(/good to see you/i);
+
+    expect(textElement).toBeNull();
     expect(linkElement).toBeInTheDocument();
   });
 });
