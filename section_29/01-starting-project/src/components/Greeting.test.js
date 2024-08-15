217992e51441999ec5cfaae216c8ef645bab7580
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-08-15 18:33:10 -0300

    542. Grouping Tests Together With Test Suites

--- section_29/01-starting-project/src/components/Greeting.test.js
+++ section_29/01-starting-project/src/components/Greeting.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('renders Greeting', () => {
-  render(<Greeting />);
-  const linkElement = screen.getByText(/Good to see you/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Greeting component', () => {
+  test('renders Greeting', () => {
+    render(<Greeting />);
+    const linkElement = screen.getByText(/Good to see you/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
