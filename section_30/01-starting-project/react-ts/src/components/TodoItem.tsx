4f487527b476c35d10a1fc9c3409e38395fae12e
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-09-09 18:35:45 -0300

    570. Time to Practice: Removing a Todo

--- section_30/01-starting-project/react-ts/src/components/TodoItem.tsx
+++ section_30/01-starting-project/react-ts/src/components/TodoItem.tsx
@@ -1,5 +1,5 @@
-const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li>{props.text}</li>
+const TodoItem: React.FC<{ text: string; onRemoveTodo: () => void }> = (props) => {
+  return <li onClick={props.onRemoveTodo}>{props.text}</li>
 }
 
 export default TodoItem;
